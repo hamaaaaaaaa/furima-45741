2884a91a04b931adf5401533fa659cf33098db57
hamaaaaaaaa <hamatoten@gmail.com>
2025-11-23 20:29:57 +0900

    文字の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,7 +101,7 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
 
-      it 'passwordが半角英数字混合でないと登録できない' do
+      it 'passwordが英字のみでは登録できない' do
         @user.password = 'abcdef'
         @user.password_confirmation = 'abcdef'
         @user.valid?
