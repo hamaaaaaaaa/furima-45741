229749879e914b18695222448a4aade21ba909d9
hamaaaaaaaa <hamatoten@gmail.com>
2025-11-24 20:27:11 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,15 @@ class Item < ApplicationRecord
 
   # User
   belongs_to :user
+
+  # バリデーション
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
