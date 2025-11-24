aff716ab6c8b133523fe895bb200181326b7871a
hamaaaaaaaa <hamatoten@gmail.com>
2025-11-24 20:30:44 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,29 @@
 class ItemsController < ApplicationController
-  def index
-  end
+  # ログインしていないユーザーはログインページへリダイレクト
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
+    @item = Item.new
   end
 
-  def show
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to root_path  # 出品成功 → トップページ
+    else
+      render :new  # バリデーションエラー → 出品ページに戻る
+    end
   end
 
-  def edit
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :item_name, :description, :price, :image,
+      :category_id, :condition_id, :shipping_charge_id,
+      :prefecture_id, :delivery_day_id
+    )
   end
 end
